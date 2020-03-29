79b37735ec576ff327a07d5857a3c83a37882682
Ryan Levi <ryan.e.levi@gmail.com>
2020-03-28 17:41:57 -0700

    fix get and post

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,13 +1,17 @@
 class AppointmentsController < ApplicationController
   @appointments = Appointment.all
 
-  def show
-    puts @appointments
-    render json: @appointments.as_json(only: [:id], include: {
+  def index
+    appointments = Appointment.all
+    render json: appointments.as_json(only: [:id, :title, :description, :start_time, :duration_in_minutes], include: {
       customer: {only: [:username, :id]},
-      client: {only: [:username, :id]},
-      only: [:title, :description, :start_time, :duration_in_minutes]
+      client: {only: [:username, :id]}
     })
+  end
+
+  def show
+    appointment = Appointment.find params[:id]
+    render json: appointment.as_json
   end
 
   def create
@@ -17,8 +21,8 @@ class AppointmentsController < ApplicationController
       a.description = params[:description]
       a.start_time = params[:start_time]
       a.duration_in_minutes = params[:duration_in_minutes]
-      a.client_id = Client.find_or_create_by username: params[:client]
-      a.customer_id = Customer.find_or_create_by username: params[:customer]
+      a.client_id = Client.find_or_create_by(username: params[:client])
+      a.customer_id = Customer.find_or_create_by(username: params[:customer])
     end
     render json: new_appointment.as_json(only: [:id])
   end
