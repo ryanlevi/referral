38a8b67caeb59730014b72e0f1889b8c62a8a079
Ryan Levi <ryan.e.levi@gmail.com>
2020-03-28 17:08:06 -0700

    post

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -9,4 +9,17 @@ class AppointmentsController < ApplicationController
       only: [:title, :description, :start_time, :duration_in_minutes]
     })
   end
+
+  def create
+    new_appointment = Appointment.create do |a|
+      a.id = params[:id]
+      a.title = params[:title]
+      a.description = params[:description]
+      a.start_time = params[:start_time]
+      a.duration_in_minutes = params[:duration_in_minutes]
+      a.client_id = Client.find_or_create_by username: params[:client]
+      a.customer_id = Customer.find_or_create_by username: params[:customer]
+    end
+    render json: new_appointment.as_json(only: [:id])
+  end
 end
